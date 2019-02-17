0f1076f3908c2a4dbb5511d2c1ec49573fdc9acd
PatrickKubica <patrickkubica@gmx.de>
2019-02-17 13:15:23 +0100

    create basic layout for website

--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,84 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Nuxt-PWA
-      </h1>
-      <h2 class="subtitle">
-        Nuxt PWA
-      </h2>
-    </div>
-  </section>
+  <div>
+    <section class="hero is-success is-fullheight">
+      <div class="hero-head">
+        <header class="navbar is-fixed-top">
+          <div class="container has-text-black">
+            <div class="navbar-brand">
+              <span
+                class="navbar-burger burger"
+                onclick="document.querySelector('.navbar-menu').classList.toggle('is-active');"
+                data-target="navbarMenuHeroC"
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+              </span>
+            </div>
+            <div id="navbarMenuHeroC" class="navbar-menu">
+              <div class="navbar-end">
+                <a class="navbar-item" href="#">Home</a>
+                <a class="navbar-item" href="#skills">Softwareentwicklung</a>
+                <a class="navbar-item" href="#experience">Berufserfahrung</a>
+                <a class="navbar-item" href="#education">Bildungsweg</a>
+                <a class="navbar-item" href="#awards">Abschluss &amp; Zertifizierungen</a>
+                <a class="navbar-item" href="#contact">Kontakt</a>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">Patrick Kubica</h1>
+          <h2 class="subtitle">Lead Software Developer at Sitewards GmbH, Frankfurt am Main</h2>
+        </div>
+      </div>
+    </section>
+    <section class="hero is-fullheight" id="skills">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">Softwareentwicklung</h1>
+        </div>
+      </div>
+    </section>
+    <section class="hero is-fullheight is-success" id="experience">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">Berufserfahrung</h1>
+        </div>
+      </div>
+    </section>
+    <section class="hero is-fullheight" id="education">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">Bildungsweg</h1>
+        </div>
+      </div>
+    </section>
+    <section class="hero is-fullheight is-success" id="awards">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">Abschluss &amp; Zertifizierungen</h1>
+        </div>
+      </div>
+    </section>
+    <section class="hero is-fullheight" id="contact">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">Kontakt</h1>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
+  components: {}
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
