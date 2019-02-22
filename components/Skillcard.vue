3498f60fd547ec8eacb7269c27146fec9f79089c
PatrickKubica <patrickkubica@gmx.de>
2019-02-22 20:46:08 +0100

    add skillcard component

---
+++ components/Skillcard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="column is-one-quarter-tablet">
+    <article class="message is-info has-text-left">
+      <div class="message-body">
+        <slot />
+      </div>
+    </article>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    jobTitle: {
+      type: String,
+      default: ''
+    },
+    company: {
+      type: String,
+      default: ''
+    },
+    date: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
