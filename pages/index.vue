22699ac8e74910b1e62f6523590a1558245170a2
PatrickKubica <patrickkubica@gmx.de>
2019-03-14 14:50:00 +0100

    add contact section

--- pages/index.vue
+++ pages/index.vue
@@ -20,9 +20,6 @@
             <a class="has-text-white" href="mailto:mail@patrickkubica.de">mail@patrickkubica.de</a>
           </div>
           <div class="socialicons">
-            <a class="has-text-white" href="mailto:mail@patrickkubica.de" aria-label="E-Mail">
-              <font-awesome-icon :icon="['fa', 'envelope']" class="fa-3x" />
-            </a>
             <a class="has-text-white" href="https://www.linkedin.com/in/patrick-kubica-002027156" aria-label="LinkedIn">
               <font-awesome-icon :icon="['fab', 'linkedin']" class="fa-3x" />
             </a>
@@ -48,52 +45,71 @@
         <jobcard job-title="Software Team Assistent" company="Sitewards GmbH" date="November 2012 - Februar 2015" />
       </div>
     </fhsection>
-    <fhsection section-id="education">
-      <div class="educationgrid">
-        <educationcard>
-          <div class="message-body">
-            <h1 class="title is-size-4-mobile">
-              Hochschule Darmstadt
-            </h1>
-            <h2 class="subtitle is-size-6-mobile">
-              Bachelor of Science Informatik
-            </h2>
-            <p>
-              Note: 1,4
-            </p>
-          </div>
-        </educationcard>
-        <educationcard>
-          <div class="message-body">
-            <h1 class="title is-size-4-mobile">
-              Bachgauschule Babenhausen
-            </h1>
-            <h2 class="subtitle is-size-6-mobile">
-              Abitur
-            </h2>
-            <p>
-              Note: 1,5
-            </p>
-          </div>
-        </educationcard>
-        <educationcard>
-          <div class="message-body">
-            <h1 class="title is-size-4-mobile">
-              Spryker Bootcamp 
-              <br>
-              Certification
-            </h1>
-          </div>
-        </educationcard>
-        <educationcard>
-          <div class="message-body">
-            <h1 class="title is-size-4-mobile">
-              Magento Certified
-              <br>
-              Developer
-            </h1>
-          </div>
-        </educationcard>
+    <span id="education" class="educationanchor" />
+    <section class="section is-medium">
+      <div class="container has-text-centered">
+        <div class="educationgrid">
+          <educationcard>
+            <div class="message-body">
+              <h1 class="title is-size-4-mobile">
+                Hochschule Darmstadt
+              </h1>
+              <h2 class="subtitle is-size-6-mobile">
+                Bachelor of Science Informatik
+              </h2>
+              <p>
+                Note: 1,4
+              </p>
+            </div>
+          </educationcard>
+          <educationcard>
+            <div class="message-body">
+              <h1 class="title is-size-4-mobile">
+                Bachgauschule Babenhausen
+              </h1>
+              <h2 class="subtitle is-size-6-mobile">
+                Abitur
+              </h2>
+              <p>
+                Note: 1,5
+              </p>
+            </div>
+          </educationcard>
+          <educationcard>
+            <div class="message-body">
+              <h1 class="title is-size-4-mobile">
+                Spryker Bootcamp 
+                <br>
+                Certification
+              </h1>
+            </div>
+          </educationcard>
+          <educationcard>
+            <div class="message-body">
+              <h1 class="title is-size-4-mobile">
+                Magento Certified
+                <br>
+                Developer
+              </h1>
+            </div>
+          </educationcard>
+        </div>
+      </div>
+    </section>
+    <fhsection section-id="contact" additional-classes="is-light has-background has-background-third">
+      <h1 class="title is-size-4-mobile is-size-1-tablet">
+        <a class="has-text-white" href="mailto:mail@patrickkubica.de">mail@patrickkubica.de</a>
+      </h1>
+      <div class="socialicons-contact">
+        <a class="has-text-white" href="https://www.linkedin.com/in/patrick-kubica-002027156" aria-label="LinkedIn">
+          <font-awesome-icon :icon="['fab', 'linkedin']" class="fa-3x" />
+        </a>
+        <a class="has-text-white" href="https://www.xing.com/profile/Patrick_Kubica2" aria-label="Xing">
+          <font-awesome-icon :icon="['fab', 'xing']" class="fa-3x" />
+        </a>
+        <a class="has-text-white" href="https://github.com/PatrickKubica" aria-label="GitHub">
+          <font-awesome-icon :icon="['fab', 'github']" class="fa-3x" />
+        </a>
       </div>
     </fhsection>
   </div>
